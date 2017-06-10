e55c35a85a1ac50d5475e2e438ad538d57a19339
clarkdenunzio <clark@clarkdenunzio.com>
2017-06-10 13:21:17 -1000

    playing around still

--- js/app.js
+++ js/app.js
@@ -1,13 +1,43 @@
 console.log('sanity check');
 
-var myBoards = ['agnarr'];
+var favoriteBoard = 'agnarr';
 var randomBoards = ['everquest', 'wow', 'leagueoflegends', 'dota', 'hearthstone'];
 
-var grabber = function(url){
+var builder = function(response){
+
+  response.data.children.forEach( function(arty){
+    var thisArty = document.createElement('div');
+    thisArty.className = 'arty-box';
+
+    var title = document.createElement('h2');
+    title.innerHTML = arty.data.title;
+    thisArty.appendChild(title);
+
+    var author = document.createElement('p');
+    author.innerHTML = "By: " + arty.data.author;
+    thisArty.appendChild(author);
+
+    document.getElementById('arty-container').appendChild(thisArty);
+  });
+
+};
+
+var clearContainer = function() {
+
+  var container = document.getElementById('arty-container');
+
+  while ( container.hasChildNodes() ){
+    container.removeChild(container.childNodes[0]);
+  }
+
+};
+
+var creator = function(url){
 
   var reqListener = function(){
     var response = JSON.parse(this.responseText);
     console.log(response);
+    builder(response);
   };
 
   var oReq = new XMLHttpRequest();
@@ -15,20 +45,28 @@ var grabber = function(url){
   oReq.open('GET', url);
   oReq.send();
 
+
+
 };
 
 var random = function(){
 
+  clearContainer();
   console.log('this is random');
+  var choice = Math.floor( Math.random() * randomBoards.length );
+  var url = "https://www.reddit.com/r/" + randomBoards[choice] + ".json";
+  console.log(url);
+  creator(url);
 
 };
 
 var favorite = function(){
 
+  clearContainer();
   console.log("this is favorite");
-  var url = "https://www.reddit.com/r/" + myBoards[0] + ".json";
+  var url = "https://www.reddit.com/r/" + favoriteBoard + ".json";
   console.log(url);
-  grabber(url);
+  creator(url);
 
 };
 
